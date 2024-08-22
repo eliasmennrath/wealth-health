8c858f070529dfcc74eea09648d1ffa46ac8547e
eliasmennrath <elias.mennrath@gmail.com>
2024-08-22 11:54:06 +0200

    Store data on submit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,35 +1,29 @@
+import { useState } from "react";
+
 import store from "../store/store";
 import save from "../store/employee";
 
+import "./home.css";
+
 export default function Home() {
 
-    // const [firstName, setFirstName] = useState("");
-    // const [lastName, setLastName] = useState("");
-    // const [dateOfBirth, setDateOfBirth] = useState("");
-    // const [startDate, setStartDate] = useState("");
-    // const [street, setStreet] = useState("");
-    // const [city, setCity] = useState("");
-    // const [state, setState] = useState("");
-    // const [zipCode, setZipCode] = useState("");
-    // const [department, setDepartment] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("");
+    const [startDate, setStartDate] = useState("");
+    const [street, setStreet] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [zipCode, setZipCode] = useState("");
+    const [department, setDepartment] = useState("");
 
-    // function handleChange(e) { 
+    function handleSubmit(e) {
+        e.preventDefault();
 
-    // }
+        if(store.dispatch(save({ firstName, lastName, dateOfBirth, startDate, department, street, city, state, zipCode }))) {
+            displayModal();
+        }
 
-
-    function handleSubmit(formData) {
-        const firstName = formData.get("first-name");
-        const lastName = formData.get("last-name");
-        const dateOfBirth = formData.get("date-of-birth");
-        const startDate = formData.get("start-date");
-        const department = formData.get("department");
-        const street = formData.get("street");
-        const city = formData.get("city");
-        const state = formData.get("state");
-        const zipCode = formData.get("zip-code");
-
-        store.dispatch(save({ firstName, lastName, dateOfBirth, startDate, department, street, city, state, zipCode }));
         
         
         // const employee = {
@@ -47,43 +41,47 @@ export default function Home() {
         // store.dispatch(save({ employee }));
     }
 
+    function displayModal() {
+
+    }
+
 
 
     return (
         <div className="container">
             {/* Link */}
             <h2>Create Employee</h2>
-            <form action={handleSubmit} id="create-employee">
-                <label htmlFor="first-name">First Name: </label>
-                <input type="text" id="first-name" name="first-name" />
+            <form action="#" id="create-employee" onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name: </label>
+                <input onChange={(e) => setFirstName(e.target.value)} type="text" id="firstName" name="firstName" />
 
-                <label htmlFor="last-name">Last Name: </label>
-                <input type="text" id="last-name" name="last-name" />
+                <label htmlFor="lastName">Last Name: </label>
+                <input onChange={(e) => setLastName(e.target.value)} type="text" id="lastName" name="lastName" />
 
-                <label htmlFor="date-of-birth">Date of Birth: </label>
-                <input id="date-of-birth" name="date-of-birth" type="text" />
+                <label htmlFor="dateOfBirth">Date of Birth: </label>
+                <input onChange={(e) => setDateOfBirth(e.target.value)} id="dateOfBirth" name="dateOfBirth" type="text" />
 
-                <label htmlFor="start-date">Start date: </label>
-                <input id="start-date" name="start-date" type="text" />
+                <label htmlFor="startDate">Start date: </label>
+                <input onChange={(e) => setStartDate(e.target.value)} id="startDate" name="startDate" type="text" />
 
                 <fieldset className="address">
                     <legend>Address</legend>
 
                     <label htmlFor="street">Street</label>
-                    <input id="street" name="street" type="text" />
+                    <input onChange={(e) => setStreet(e.target.value)} id="street" name="street" type="text" />
 
                     <label htmlFor="city">City</label>
-                    <input id="city" name="city" type="text" />
+                    <input onChange={(e) => setCity(e.target.value)} id="city" name="city" type="text" />
 
                     <label htmlFor="state">State</label>
-                    <select name="state" id="state"></select>
+                    <select onChange={(e) => setState(e.target.value)} name="state" id="state"></select>
 
-                    <label htmlFor="zip-code">Zip Code</label>
-                    <input id="zip-code" name="zip-code" type="number" />
+                    <label htmlFor="zipCode">Zip Code</label>
+                    <input onChange={(e) => setZipCode(e.target.value)} id="zipCode" name="zipCode" type="number" />
                 </fieldset>
 
                 <label htmlFor="department">Department: </label>
-                <select name="department" id="department">
+                <select onChange={(e) => setDepartment(e.target.value)} name="department" id="department">
                     <option>Sales</option>
                     <option>Marketing</option>
                     <option>Engineering</option>
